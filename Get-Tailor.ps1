e1e3968e5009d521f54cc1a81659181024b344a5
McLeopold <mcleopold@gmail.com>
2017-02-15 22:20:42 -0800

    Add Get-Tailor command
    Initial commit of Get-Tailor command.

---
+++ Get-Tailor.ps1
@@ -0,0 +1,43 @@
+﻿function Get-Tailor {
+    param
+    (
+        [string[]]$files
+    )
+
+    function highlight
+    {
+        Begin
+        {
+            $settings = Get-Content "Tailor.json" | ConvertFrom-Json
+        }
+        Process
+        {
+            $matched = $false
+            foreach ($setting in $settings.rules) {
+                if ($_ -match $setting.match) {
+                    $matched = $true
+                    Write-Host $_ -ForegroundColor $setting.color
+                    break
+                }
+            }
+            if (-not $matched -and $settings.default.enabled)
+            {
+                Write-Host $_ -ForegroundColor $settings.default.color
+            }
+        }
+    }
+
+    workflow tailor
+    {
+        param
+        (
+            [string[]]$files
+        )
+        foreach -parallel ($file in $files) 
+        {
+            Get-Content -Tail 1 $file -wait
+        }
+    }
+
+    tailor $files | highlight
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # TailorPosh
 Tail Utility for Powershell
+
+## Purpose
+This utility will allow you to tail multiple files at the same time.  Setting can be configured with the Tailor.json file.
+
+Each line of the file can be checked against a regular expression to add optional highlighting or to be filtered out.
+The default rules will highlight the following lines in a different color:
+
+  * DEBUG
+  * INFO
+  * WARNING
+  * FATAL
+
+To exclude lines that don't match any rules, set the default.enabled setting to false in the Tailor.json file.
+
+## Use
+
+Dot source the file in a session of Powershell:
+
+    . ".\Get-Tailor.ps1"
+
+Call the command with a parameter array of file names:
+
+    Get-Tailor -files one.txt, two.txt
